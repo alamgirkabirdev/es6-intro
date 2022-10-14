993cf2ca4155688cc9b6e03c4123e2a88bfc1ec6
Alamgir kabir <alamgir.saguli@gmail.com>
2022-10-14 16:02:19 +0600

    2nd added

---
+++ let-const.js
@@ -0,0 +1,20 @@
+let balance = 1240;
+balance = 1340;
+const userName = 'janpahi potas potas';
+const weTogether = 'ami' + userName;
+console.log(weTogether);
+// userName = 'moyna pakhi';
+
+const numbers =[4545, 54, 74, 87,10 ];
+// numbers = [99, 45, 32, 94];  (not allow)
+numbers.push(555);
+numbers[1]= 333;
+
+for(let i = 0; i<numbers.length; i++){
+    const number = numbers[i];
+    console.log(number);
+}
+
+const student = { roll: 101, name:'mofij', job:'intern'};
+student.name = 'mofazzol';
+// student = {name:'mofazzol'};
