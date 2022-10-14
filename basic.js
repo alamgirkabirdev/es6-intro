344adc2d0e42f15abbcaf8e3ef74c0b71a148e13
Alamgir kabir <alamgir.saguli@gmail.com>
2022-10-14 10:13:56 +0600

    first commit of es6-intro

---
+++ basic.js
@@ -0,0 +1,50 @@
+// varible
+var deposit = 400;
+
+// condition(
+if(deposit> 500){
+
+}
+else if(deposit < 200){
+
+}
+else if(deposit == 500){
+
+}
+else if(deposit != 500){
+
+}
+else if(deposit <= 500){
+
+}
+else (deposit >= 500){
+
+}
+
+// array
+const numbers = [45, 12, 10, 9, 94, 42, 75];
+const numberCount = number.length;
+numbers.push(1250);
+numbers.pop();
+numbers[2]=555;
+// check whether 222 included in the array
+if(numbers.indexOf != -1){
+
+}
+if(numbers.includes(222)){
+
+}
+// loop
+//while, for
+for(const number of numbers){
+
+}
+// function
+function fullName(first, second){
+    const name = first +' '+ second;
+    console.log(name);
+}
+const person = fullName('Allah', 'Akbar');
+
+// object
+const bottle = {color:'yellow', contain:'water', price:'120'}
